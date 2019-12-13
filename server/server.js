6a25e9b9430ed2b4a18fc9a6cf4276f200789066
DWS-paris <j.noyer@digitalworkshop.fr>
2019-12-13 18:05:24 +0100

    Ajout d'un peu de design

--- server/server.js
+++ server/server.js
@@ -47,18 +47,6 @@ Config
             */
                 // Homepage
                 server.get( '/', (req, res) => res.render('index', { title: 'NodeJS simple API' }) );
-
-                // Add post
-                server.get( '/add-post', (req, res) => res.render('add-post', { title: 'NodeJS simple API' }) );
-
-                // Single post: call the route like this: localhost:2767/post?id=1
-                server.get( '/post', (req, res) => res.render('post', { title: 'NodeJS simple API' }) );
-
-                // Edit post: call the route like this: localhost:2767/edit-post?id=1
-                server.get( '/edit-post', (req, res) => res.render('edit-post', { title: 'NodeJS simple API' }) );
-
-                // Register page
-                server.get( '/register', (req, res) => res.render('register', { title: 'NodeJS simple API' }) );
             //
 
             /*
